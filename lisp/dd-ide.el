8518a9deee7c960c51f4b3b2b9de860cfa748b38
David Duthie <dave.a.duthie@gmail.com>
2023-11-06 08:08:56 +0200

    Tweak eglot

--- lisp/dd-ide.el
+++ lisp/dd-ide.el
@@ -1,7 +1,10 @@
 (use-package eglot
   :bind (("C-, a" . #'eglot-code-actions)
          ("C-, r" . #'eglot-rename))
-  :hook ((clojure-mode clojure-ts-mode rust-mode terraform-mode) . eglot-ensure))
+  :hook ((clojure-mode clojure-ts-mode rust-mode terraform-mode) . eglot-ensure)
+  :config
+  (setq eglot-sync-connect 0)
+  (setq eglot-autoshutdown t))
 
 (use-package xref
   :bind (("M-." . #'xref-find-definitions)
