53648285e6a1f03669b0a8aa01919eda0b6093e9
David Duthie <dave.a.duthie@gmail.com>
2023-11-11 17:10:47 +0200

    Display magit in a pop up window

--- lisp/dd-ui.el
+++ lisp/dd-ui.el
@@ -30,18 +30,18 @@
 (setq display-buffer-alist
       (list
        ;; Show shells at the bottom of the frame
-       '("shell\\*$" display-buffer-in-direction
-	 (direction . bottom)
-	 (window . root)
+       '("shell\\*$" display-buffer-in-side-window
+	 (side . bottom)
+	 ;; (window . root)
 	 (window-height . 0.3))
 
        ;; Compilation output, grep, references, etc. go to the right
        `(,(rx (| "*compilation*" "*grep*" "*xref*"))
-         display-buffer-in-side-window
-         (side . right)
-         (slot . 0)
-         (window-parameters . ((no-delete-other-windows . t)))
-         (window-width . 80))
+	 display-buffer-in-side-window
+	 (side . right)
+	 (slot . 0)
+	 (window-parameters . ((no-delete-other-windows . t)))
+	 (window-width . 0.4))
 
        ;; Show directory browser in a left side bar
        '((derived-mode . dired-mode)
@@ -49,22 +49,23 @@
 	 (side . left)
 	 (slot . 0)
 	 (window-parameters . ((no-delete-other-windows . t)))
-	 (window-width . 80))
+	 (window-width . 0.3))
 
        ;; test files go to the right
        `("[-_]test"
-         display-buffer-in-direction
-         (direction . right))
+	 display-buffer-in-direction
+	 (direction . right)
+	 (window-width . 0.5))
 
        ;; Magit
-       `("^magit"
-	 display-buffer-in-direction
+       `("^magit:$"
+	 display-buffer-pop-up-window
 	 (direction . right))
-       
+
        '((or (derived-mode . org-mode)
 	     "\\*Org Agenda\\*")
-         (display-buffer-in-tab display-buffer-in-direction)
-         (tab-name . "🚀 Org"))
+	 (display-buffer-in-tab display-buffer-in-direction)
+	 (tab-name . "🚀 Org"))
        ;;
        ))
 
