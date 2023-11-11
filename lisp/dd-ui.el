f94ae161a248d95c912d1e5e31a64e5080b56ea7
David Duthie <dave.a.duthie@gmail.com>
2023-11-11 15:48:33 +0200

    Make some buffer rules

--- lisp/dd-ui.el
+++ lisp/dd-ui.el
@@ -21,4 +21,50 @@
 (setq frame-resize-pixelwise t)
 (setq window-resize-pixelwise t)
 
+;;; Buffer rules
+
+(setq switch-to-buffer-in-dedicated-window 'pop)
+(setq switch-to-buffer-obey-display-actions t)
+(setq window-sides-slots '(1 1 1 1))
+
+(setq display-buffer-alist
+      (list
+       ;; Show shells at the bottom of the frame
+       '("shell\\*$" display-buffer-in-direction
+	 (direction . bottom)
+	 (window . root)
+	 (window-height . 0.3))
+
+       ;; Compilation output, grep, references, etc. go to the right
+       `(,(rx (| "*compilation*" "*grep*" "*xref*"))
+         display-buffer-in-side-window
+         (side . right)
+         (slot . 0)
+         (window-parameters . ((no-delete-other-windows . t)))
+         (window-width . 80))
+
+       ;; Show directory browser in a left side bar
+       '((derived-mode . dired-mode)
+	 display-buffer-in-side-window
+	 (side . left)
+	 (slot . 0)
+	 (window-parameters . ((no-delete-other-windows . t)))
+	 (window-width . 80))
+
+       ;; test files go to the right
+       `("[-_]test"
+         display-buffer-in-direction
+         (direction . right))
+
+       ;; Magit
+       `("^magit"
+	 display-buffer-full-frame)
+       
+       '((or (derived-mode . org-mode)
+	     "\\*Org Agenda\\*")
+         (display-buffer-in-tab display-buffer-in-direction)
+         (tab-name . "🚀 Org"))
+       ;;
+       ))
+
 (provide 'dd-ui)
