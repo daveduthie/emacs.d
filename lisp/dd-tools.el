0e633f15a64ffb1b97311b1185cad0a63618abaa
David Duthie <dave.a.duthie@gmail.com>
2023-12-14 20:34:01 +0200

    Advise vterm to start at project root

--- lisp/dd-tools.el
+++ lisp/dd-tools.el
@@ -5,6 +5,12 @@
   (setq browse-at-remote-prefer-symbolic nil))
 
 (use-package vterm
-  :bind (("C-x p v" . vterm)))
+  :bind (("C-x p v" . vterm))
+  :config
+  (define-advice vterm
+      (:around (orig-fun &rest args) dd/project-vterm)
+    (setq default-directory (project-root (project-current)))
+    (setq vterm-buffer-name (concat "*vterm " (project-name (project-current)) "*"))
+    (apply orig-fun args)))
 
 (provide 'dd-tools)
