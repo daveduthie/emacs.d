0e2822f9e75e755d86d20689b191ffd157a43ae2
David Duthie <dave.a.duthie@gmail.com>
2023-12-14 20:31:50 +0200

    Clojure utils

--- lisp/dd-clojure.el
+++ lisp/dd-clojure.el
@@ -5,16 +5,62 @@
   :config
   (setq clojure-toplevel-inside-comment-form t))
 
-(use-package cider
-  :hook (clojure-mode . cider-mode)
-  :config
-  (setq markdown-indent-on-enter nil)
-  ;; Revisit?
-  (setq cider-xref-fn-depth 90))
-
 (use-package zprint-format
   :commands (zprint-format-buffer
 	     zprint-format-region
 	     zprint-format-on-save-mode))
+
+;; def portal to the dev namespace to allow dereferencing via @dev/portal
+(defun portal.api/open ()
+  (interactive)
+  (cider-nrepl-sync-request:eval
+    "(do (ns dev) (def portal ((requiring-resolve 'portal.api/open))) (add-tap (requiring-resolve 'portal.api/submit)))"))
+
+(defun portal.api/clear ()
+  (interactive)
+  (cider-nrepl-sync-request:eval "(portal.api/clear)"))
+
+(defun portal.api/close ()
+  (interactive)
+  (cider-nrepl-sync-request:eval "(portal.api/close)"))
+
+;;;; Sync deps - not quite in working order
+
+(defun dd/sync-deps-aliases (aliases)
+  (apply #'concat
+	 "[ "
+	 (append (mapcar (lambda (x) (concat ":" x " ")) (split-string aliases " "))
+		 '("]"))))
+
+(defun dd/sync-deps (&optional aliases)
+  (interactive (list (completing-read "Aliases: " nil)))
+  (message
+   (cider-nrepl-sync-request:eval
+    (format "((requiring-resolve 'clojure.repl.deps/sync-deps) %s)"
+	    (dd/sync-deps-aliases aliases)))))
+
+;; Eval text register
+
+(defun dd/cider-eval-register ()
+  (interactive)
+  (let ((register-text (with-temp-buffer
+			 (if (fboundp 'consult-register)
+			     (consult-register)
+			   (insert-register))
+			 (buffer-string))))
+    (message (cider-nrepl-sync-request:eval register-text))))
+
+(use-package cider
+  :hook (clojure-mode . cider-mode)
+  :bind (:map clojure-mode-map
+	      ("C-c t p o" . #'portal.api/open)
+	      ("C-c t p c" . #'portal.api/clear)
+	      ("C-c t p q" . #'portal.api/close)
+	      ;; TODO: bind dd/cider-eval-register
+	      )
+  :config
+  (setq markdown-indent-on-enter nil)
+  ;; Revisit?
+  (setq cider-xref-fn-depth 90))
 
 (provide 'dd-clojure)
