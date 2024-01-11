d34d7907e2e60ad6ac8724a947c0f1f581f14d3d
David Duthie <dave.a.duthie@gmail.com>
2024-01-11 07:11:00 +0200

    Configure some more packages

--- lisp/dd-misc-lang.el
+++ lisp/dd-misc-lang.el
@@ -1,4 +1,5 @@
 ;; -*- lexical-binding: t; -*-
+
 (use-package terraform-mode
   :mode ("\\.tf\\'" . terraform-mode)
   :hook (terraform-mode . eglot-ensure))
@@ -7,6 +8,19 @@
   :mode ("\\.md\\'" . markdown-mode))
 
 (use-package js
-  :mode ("\\.m?jsx?\\'" . js-mode))
+  :mode ("\\.m?jsx?\\'" . js-mode)
+  :hook (js-mode . eglot-ensure))
+
+(use-package yaml-ts-mode
+  :mode ("\\.ya?ml\\'"))
+
+(use-package lua-ts-mode
+  :mode ("\\.lua\\'"))
+
+(use-package graphql-ts-mode
+  :mode ("\\.graphql\\'" "\\.gql\\'")
+  :config
+  (add-to-list 'treesit-language-source-alist
+               '(graphql "https://github.com/bkegley/tree-sitter-graphql")))
 
 (provide 'dd-misc-lang)
