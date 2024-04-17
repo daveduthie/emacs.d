d567930b693fe51e9e0749be8169207d0245f8ec
David Duthie <dave.a.duthie@gmail.com>
2024-04-17 16:29:30 +0200

    Try out dirvish

--- lisp/dd-tools.el
+++ lisp/dd-tools.el
@@ -56,10 +56,85 @@
 			:stream t
 			:models '("mistral:latest"))))
 
-(use-package codeium
-  :defer t
+(use-package dirvish
+  :load-path "lib/dirvish/extensions"
+  :init
+  (dirvish-override-dired-mode)
+  :custom
+  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+   '(("h" "~/"                          "Home")
+     ("d" "~/Downloads/"                "Downloads")
+     ("m" "~/src"                       "src")))
   :config
-  (setq codeium-mode-line-enable (lambda (api) (not (memq api '(CancelRequest Heartbeat AcceptCompletion)))))
-  (add-to-list 'mode-line-format '(:eval (car-safe codeium-mode-line)) t))
+  (require 'dirvish-fd)
+  (require 'dirvish-side)
+  (require 'dirvish-history)
+  (require 'dirvish-emerge)
+  ;; (dirvish-peek-mode) ; Preview files in minibuffer
+  (dirvish-side-follow-mode)	   ; similar to `treemacs-follow-mode'
+  (setq dirvish-mode-line-format '(:left (sort symlink) :right (omit yank index)))
+  (setq dirvish-attributes '(all-the-icons file-time file-size collapse subtree-state vc-state git-msg))
+  (setq delete-by-moving-to-trash t)
+  (setq dired-listing-switches "-l --almost-all --human-readable --group-directories-first --no-group")
+
+  ;; https://github.com/alexluigit/dirvish/pull/257
+  (defun dirvish-dired-noselect-a (fn dir-or-list &optional flags)
+    "Return buffer for DIR with FLAGS, FN is `dired-noselect'."
+    (let* ((dir (if (consp dir-or-list) (car dir-or-list) dir-or-list))
+	   (key (file-name-as-directory (expand-file-name dir)))
+	   (this dirvish--this)
+	   (dv (if (and this (eq this-command 'dired-other-frame)) (dirvish-new)
+		 (or this (car (dirvish--find-reusable)) (dirvish-new))))
+	   (bname buffer-file-name)
+	   (remote (file-remote-p dir))
+	   (flags (or flags (dv-ls-switches dv)))
+	   (buffer (alist-get key (dv-roots dv) nil nil #'equal))
+	   (new-buffer-p (not buffer)))
+      (if this (set-window-dedicated-p nil nil) (setcar (dv-layout dv) nil))
+      (when new-buffer-p
+	(if (not remote)
+	    (let ((dired-buffers nil))	; disable reuse from dired
+	      (setq buffer (apply fn (list dir-or-list flags))))
+	  (require 'dirvish-extras)
+	  (setq buffer (dirvish-noselect-tramp fn dir-or-list flags remote)))
+	(with-current-buffer buffer (dirvish-init-dired-buffer))
+	(push (cons key buffer) (dv-roots dv))
+	(push (cons key buffer) dired-buffers))
+      (with-current-buffer buffer
+	(cond (new-buffer-p nil)
+	      ((and (not remote) (not (equal flags dired-actual-switches)))
+	       (dired-sort-other flags))
+	      ((eq dired-auto-revert-buffer t) (revert-buffer))
+	      ((functionp dired-auto-revert-buffer)
+	       (when (funcall dired-auto-revert-buffer dir) (revert-buffer))))
+	(dirvish-prop :dv (dv-name dv))
+	(dirvish-prop :gui (display-graphic-p))
+	(dirvish-prop :remote remote)
+	(dirvish-prop :root key)
+	(when bname (dired-goto-file bname))
+	(setf (dv-index dv) (cons key buffer))
+	(run-hook-with-args 'dirvish-find-entry-hook key buffer)
+	buffer)))
+
+  :bind	     ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
+  (("C-c f" . dirvish-fd)
+   :map dirvish-mode-map	   ; Dirvish inherits `dired-mode-map'
+   ("a"   . dirvish-quick-access)
+   ("f"   . dirvish-file-info-menu)
+   ("y"   . dirvish-yank-menu)
+   ("N"   . dirvish-narrow)
+   ("^"   . dired-up-directory)
+   ("h"   . dirvish-history-jump)	; remapped `describe-mode'
+   ("s"   . dirvish-quicksort)	; remapped `dired-sort-toggle-or-edit'
+   ("v"   . dirvish-vc-menu)	; remapped `dired-view-file'
+   ("TAB" . dirvish-subtree-toggle)
+   ("M-f" . dirvish-history-go-forward)
+   ("M-b" . dirvish-history-go-backward)
+   ("M-l" . dirvish-ls-switches-menu)
+   ("M-m" . dirvish-mark-menu)
+   ("M-t" . dirvish-layout-toggle)
+   ("M-s" . dirvish-setup-menu)
+   ("M-e" . dirvish-emerge-menu)
+   ("M-j" . dirvish-fd-jump)))
 
 (provide 'dd-tools)
