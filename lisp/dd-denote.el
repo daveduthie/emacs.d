2ec57349b1fd9ab3e4a2fe3a0015f7114bab5758
David Duthie <dave.a.duthie@gmail.com>
2024-08-02 08:17:46 +0200

    Combine denote directory with old zk directory

--- lisp/dd-denote.el
+++ lisp/dd-denote.el
@@ -1,6 +1,6 @@
 (use-package denote
   :config
-  (setq denote-directory (expand-file-name "~/Documents/notes/"))
+  (setq denote-directory (expand-file-name "~/Documents/org/zk"))
   (setq denote-known-keywords nil)
   (setq denote-journal-extras-title-format 'day-date-month-year)
   (setq denote-date-prompt-use-org-read-date t)
